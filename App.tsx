ee1b09d472322cbcb7cf3586a6efec03f785280f
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2022-11-06 19:16:10 +0000

    animated list

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from "react-native-reanimated";
+import { ViewToken } from "react-native";
+import { useCallback } from "react";
+
+const data = new Array(50).fill(0).map((_, index) => ({ id: index }));
+
+type ListItemProps = {
+  viewAbleItems: Animated.SharedValue<ViewToken[]>;
+  item: {
+    id: number;
+  };
+};
+
+const ListItems = ({ viewAbleItems, item }: ListItemProps) => {
+  const rStyle = useAnimatedStyle(() => {
+    const isVisible = Boolean(
+      viewAbleItems.value
+        .filter(item => item.isViewable)
+        .find(viewAbleItems => viewAbleItems.item.id === item.id)
+    );
+
+    return {
+      opacity: withTiming(isVisible ? 1 : 0),
+      transform: [
+        {
+          scale: withTiming(isVisible ? 1 : 0.6)
+        }
+      ]
+    };
+  }, []);
+
+  return <Animated.View style={[styles.card, rStyle]} />;
+};
+
+const viewabilityConfig = {
+  waitForInteraction: true,
+  viewAreaCoveragePercentThreshold: 80
+};
+
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+}
 
 export default function App() {
+  const viewAbleItems = useSharedValue<ViewToken[]>([]);
+
+  const handleViewableItemsChanged = useCallback(
+    ({ viewableItems }: ChangeImageProps) => {
+      viewAbleItems.value = viewableItems;
+    },
+    []
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          contentContainerStyle={{ paddingTop: 40 }}
+          viewabilityConfig={viewabilityConfig}
+          onViewableItemsChanged={handleViewableItemsChanged}
+          renderItem={({ item }) => {
+            return <ListItems item={item} viewAbleItems={viewAbleItems} />;
+          }}
+        />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 24
   },
+  card: {
+    height: 80,
+    width: "100%",
+    backgroundColor: "#78cad2",
+    marginTop: 20,
+    borderRadius: 15
+  }
 });
